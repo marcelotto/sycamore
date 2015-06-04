a9db688f5d921ac1a2a3bb717ccb820fb94bb0c0
Marcel Otto <marcelotto@gmx.de>
2015-06-04 19:20:23 +0200

    Remove old uncommented include_tree_part matcher code

--- spec/support/matchers/include_tree_part.rb
+++ spec/support/matchers/include_tree_part.rb
@@ -2,21 +2,6 @@ RSpec::Matchers.define :include_tree_part do |part|
   match do |this_tree|
     expect(this_tree).to be_a Sycamore::Tree
     expect(this_tree).to include part
-=begin
-    case part
-      when Hash
-        # part.each do |node, child|
-        #   expect(this_tree).to include_tree_part node
-        #   expect(this_tree.child(node)).to include_tree_part child
-        # end
-      when Enumerable then nodes = part
-        nodes.each do |node|
-          expect(this_tree).to include_tree_part node
-        end
-      else node = part
-        expect(this_tree).to include node
-    end
-=end
   end
 end
 
