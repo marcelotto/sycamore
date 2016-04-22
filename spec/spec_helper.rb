2be48cf1c9c922afb41794acd2235258984c34a7
Marcel Otto <marcelotto@gmx.de>
2016-04-22 18:05:44 +0200

    chore: clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,51 +15,14 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sycamore'
-require 'set'
 
 SPEC_DIR = File.dirname(__FILE__)
-
-Dir[File.join(SPEC_DIR, 'scratch/support/**/*.rb')].each {|f| require f }
 Dir[File.join(SPEC_DIR, 'support/**/*.rb')].each {|f| require f }
 
 RSpec::Matchers.define_negated_matcher :be_different_to, :be
-
-# TODO: How can we implement this without global variables with RSpec?
-
-$last_number = 0
-$last_string = 0
-$last_symbol = 0
-
-def number
-  $last_number += 1
-end
-alias another_number number
-
-def last_number
-  $last_number
-end
-
-def string
-  "string#{$last_string += 1}"
-end
-alias another_string string
-
-def last_string
-  $last_string
-end
-
-def symbol
-  "symbol#{$last_symbol += 1}".to_sym
-end
-alias another_symbol symbol
-
-def last_symbol
-  $last_symbol
-end
 
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
 
   config.example_status_persistence_file_path = './spec/examples.txt'
 end
-
